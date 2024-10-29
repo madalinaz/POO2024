f6fb952c4661a9d2c76df7f7621dc9a7d0807489
Madalina Zurini <madalina.zurini@csie.ase.ro>
2024-10-29 15:01:43 +0200

    C05 update

--- SeriaD/C05/FileName.cpp
+++ SeriaD/C05/FileName.cpp
@@ -22,9 +22,25 @@ public:
 		this->facultate = _facultate;
 		if (_note != nullptr && _nrNote > 0) {
 			this->nrNote = _nrNote;
-			this->note = new int[this->nrNote];//alocare zm
+			this->note = new int[this->nrNote];//alocare zm pe heap
 			for (int i = 0; i < this->nrNote; i++)
 				this->note[i] = _note[i];
+		}
+		else {
+			this->nrNote = 0;
+			this->note = nullptr;
+		}
+	}
+
+	Student(const Student& s) {
+		cout << "\nAPEL CONSTRUCTOR COPIERE";
+		this->nume = s.nume;
+		this->facultate = s.facultate;
+		if (s.note != nullptr && s.nrNote > 0) {
+			this->nrNote = s.nrNote;
+			this->note = new int[this->nrNote];//alocare zm pe heap
+			for (int i = 0; i < this->nrNote; i++)
+				this->note[i] = s.note[i];
 		}
 		else {
 			this->nrNote = 0;
@@ -42,9 +58,41 @@ public:
 			cout << this->note[i] << " ";
 		cout << "\n^^^^^^^^^^^^^^^^^^^^^";
 	}
+
+	void afisare2();//antetul metodei care nu este metoda inline
+	void afisare3();
+
+	~Student() {
+		cout << "\nAPEL DESTRUCTOR";
+		if (this->note != nullptr) {
+			delete[] this->note;
+			this->note = nullptr;
+		}
+	}
 };
+
+void Student::afisare2() {
+	cout << "\n^^^^^^^^^^^^^^^^^^^^^";
+	cout << "\nNume: " << this->nume;
+	cout << "\nFacultate: " << this->facultate;
+	cout << "\nNr note: " << this->nrNote;
+	cout << "\n^^^^^^^^^^^^^^^^^^^^^";
+}
+
+void Student::afisare3() {
+	cout << "\n^^^^^^^^^^^^^^^^^^^^^";
+	cout << "\nNume: " << this->nume;
+	cout << "\nFacultate: " << this->facultate;
+	cout << "\n^^^^^^^^^^^^^^^^^^^^^";
+}
 
 int main() {
 	Student s;
+	int note[] = { 10,5,3,7 };
+	Student s2("Gigel", "CSIE", note, 4);
+	s2.afisare();
+	Student s3(s2);
+	s3.afisare();
+	s3.afisare2();
 	return 0;
 }
