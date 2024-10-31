2d05138a1850992739c49e61bf0e3cc3ce2b6bc3
Madalina Zurini <madalina.zurini@csie.ase.ro>
2024-10-31 13:44:10 +0200

    S05

---
+++ G1065/S05/FileName.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class FelMancare {
+	const int cod;
+	char* denumire;
+	int nrCalorii;
+	float pret;
+	static int nrFeluriMancare;
+	static float pretMinim;
+
+public:
+	//constructor fara param
+	//
+};
+
+int main() {
+	return 0;
+}
