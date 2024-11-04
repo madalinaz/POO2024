930dadd1697b77e7720db221397a017b0dd09d7a
Madalina Zurini <madalina.zurini@csie.ase.ro>
2024-11-04 10:40:09 +0200

    S05

---
+++ G1066/S05/FileName.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+enum Departament {
+	IT, HR, VZ
+};
+
+class Angajat {
+	const int id;//cod unic care se genereaza automat
+	char* nume;
+	float salariu;
+	Departament departament;
+	static int nrAngajati;
+
+public:
+	//constr fara param
+	//constr cu toti param
+	//constr de copiere
+	//op =
+	//get si set pentru nume
+	//meth statice pentru atributul static
+	//meth afisare
+	//destructor
+
+};
+
+int main() {
+	return 0;
+}
