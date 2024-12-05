89bb4f2a3b42367c5ca8b7ba5e51b5295ce69647
Madalina Zurini <madalina.zurini@csie.ase.ro>
2024-12-05 14:56:03 +0200

    S10

--- G1065/S10/FileName.cpp
+++ G1065/S10/FileName.cpp
@@ -91,44 +91,112 @@ public:
 };
 
 class WishList {
-	string posesor;
-	int nrCadouri;
-	Cadou** listaCadouri;
+	string posesor="Anonim";
+	int nrCadouri=0;
+	Cadou** listaCadouri=nullptr;//vector alocat dinamic de pointeri de Cadou
+
+public:
+	WishList(){ }
+
+	WishList(string _posesor, int _nrCadouri, Cadou** _listaCadouri) {
+		this->posesor = _posesor;
+		if (_nrCadouri > 0 && _listaCadouri != nullptr) {
+			this->nrCadouri = _nrCadouri;
+			this->listaCadouri = new Cadou * [this->nrCadouri];//NU se apeleaza niciun constructor din Cadou
+			for (int i = 0; i < this->nrCadouri; i++)
+				this->listaCadouri[i] = new Cadou(*_listaCadouri[i]);//apel constructor copiere din Cadou
+		}
+	}
+
+	WishList(const WishList& w) {
+		this->posesor = w.posesor;
+		if (w.nrCadouri > 0 && w.listaCadouri != nullptr) {
+			this->nrCadouri = w.nrCadouri;
+			this->listaCadouri = new Cadou * [this->nrCadouri];//NU se apeleaza niciun constructor din Cadou
+			for (int i = 0; i < this->nrCadouri; i++)
+				this->listaCadouri[i] = new Cadou(*w.listaCadouri[i]);//apel constructor copiere din Cadou
+		}
+	}
+
+	WishList& operator=(const WishList& w) {
+		if (this != &w) {
+			if (this->listaCadouri != nullptr) {
+				for (int i = 0; i < this->nrCadouri; i++) {
+					delete this->listaCadouri[i];//apel destructor din Cadou
+					this->listaCadouri[i] = nullptr;
+				}
+				delete[] this->listaCadouri;
+				this->listaCadouri = nullptr;
+			}
+			this->posesor = w.posesor;
+			if (w.nrCadouri > 0 && w.listaCadouri != nullptr) {
+				this->nrCadouri = w.nrCadouri;
+				this->listaCadouri = new Cadou * [this->nrCadouri];//NU se apeleaza niciun constructor din Cadou
+				for (int i = 0; i < this->nrCadouri; i++)
+					this->listaCadouri[i] = new Cadou(*w.listaCadouri[i]);//apel constructor copiere din Cadou
+			}
+			else {
+				this->listaCadouri = nullptr;
+				this->nrCadouri = 0;
+			}
+		}
+		return *this;
+	}
+
+	~WishList() {
+		if (this->listaCadouri != nullptr) {
+			for (int i = 0; i < this->nrCadouri; i++) {
+				delete this->listaCadouri[i];//apel destructor din Cadou
+				this->listaCadouri[i] = nullptr;
+			}
+			delete[] this->listaCadouri;
+			this->listaCadouri = nullptr;
+		}
+	}
+
+	friend ostream& operator<<(ostream& out, const WishList& w) {
+		out << "\nPosesor: " << w.posesor;
+		out << "\nNr cadouri: " << w.nrCadouri;
+		out << "\nLista cadouri: ";
+		for (int i = 0; i < w.nrCadouri; i++)
+			out << *w.listaCadouri[i];
+		return out;
+	}
 };
 
 class WishList2 {
 	string posesor;
 	int nrCadouri;
-	Cadou* listaCadouri[100];
+	Cadou* listaCadouri[100];//vector static de Cadou*
 };
 
 class WishList3 {
 	string posesor;
 	int nrCadouri;
-	Cadou* listaCadouri;
+	Cadou* listaCadouri;//vector dinamic de Cadou
 };
 
 class WishList4 {
 	string posesor;
 	int nrCadouri;
-	Cadou listaCadouri[100];
+	Cadou listaCadouri[100];//vector static de Cadou
 };
 
 class WishList5 {
 	string posesor;
-	Cadou* cadouSpecial;
+	Cadou* cadouSpecial;//pointer la un Cadou
 };
 
 class WishList6 {
 	string posesor;
-	Cadou cadouSpecial;
+	Cadou cadouSpecial;//un obj de tip Cadou
 };
 
 class WishList7 {
 	string posesor;
 	int nrPrieteni;
 	int* nrCadouri;
-	Cadou*** listaCadouri;
+	Cadou*** listaCadouri;//
 };
 
 class WishList8 {
@@ -139,6 +207,20 @@ class WishList8 {
 };
 
 int main() {
+	Cadou c1("stilou", 450, CUTIE);
+	Cadou c2("carte", 100, HARTIE);
+	Cadou c3("geanta", 1200, CUTIE);
+
+	Cadou* lista[] = { &c1,&c2,&c3, new Cadou("inel",2000,GLOB) };//refer obj Cadou si de pe stiva si de pe heap
+	cout << *lista[3];
+
+	WishList w1("Gigel", 4, lista);
+	cout << w1;
+
+	delete lista[3];
+
+	//sa se modeleze relatia has a intre WishList si 
+	// Cadou astfel incat sa implice o alocare contigua de zona de memorie
 
 	return 0;
 }
