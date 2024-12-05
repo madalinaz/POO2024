14379f29469729b0751ce586e5f134b888c1c13e
Madalina Zurini <madalina.zurini@csie.ase.ro>
2024-12-05 15:14:03 +0200

    S10

---
+++ G1059/S10/FileName.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+enum Ambalaj {
+	CUTIE, HARTIE, GLOB, PLIC
+};
+
+ostream& operator<<(ostream& out, Ambalaj a) {
+	switch (a)
+	{
+	case CUTIE:
+		out << "Cutie";
+		break;
+	case HARTIE:
+		out << "Hartie";
+		break;
+	case GLOB:
+		out << "Glob";
+		break;
+	case PLIC:
+		out << "Plic";
+		break;
+	default:
+		out << "Lipsa";
+		break;
+	}
+	return out;
+}
+
+istream& operator>>(istream& in, Ambalaj& a) {
+	int x;
+	cin >> x;
+	switch (x)
+	{
+	case 0:
+		a = Ambalaj::CUTIE;
+		break;
+	case 1:
+		a = Ambalaj::HARTIE;
+		break;
+	case 2:
+		a = Ambalaj::GLOB;
+		break;
+	case 3:
+		a = Ambalaj::PLIC;
+		break;
+	default:
+		a = Ambalaj::HARTIE;
+		break;
+	}
+	return in;
+}
+
+//Cadou has a Ambalaj
+class Cadou {
+	string denumire = "-";
+	float pret = 0;
+	Ambalaj ambalaj = Ambalaj::HARTIE;
+
+public:
+	Cadou() { }
+
+	Cadou(string _denumire, float _pret, Ambalaj _ambalaj) :denumire(_denumire), pret(_pret), ambalaj(_ambalaj) { }
+
+	Cadou(const Cadou& c) :denumire(c.denumire), pret(c.pret), ambalaj(c.ambalaj) { }
+
+	Cadou& operator=(const Cadou& c) {
+		this->denumire = c.denumire;
+		this->pret = c.pret;
+		this->ambalaj = c.ambalaj;
+		return *this;
+	}
+
+	friend ostream& operator<<(ostream& out, const Cadou& c) {
+		out << "\nDenumire: " << c.denumire;
+		out << ", pret: " << c.pret;
+		out << ", ambalaj: " << c.ambalaj;
+		return out;
+	}
+
+	friend istream& operator>>(istream& in, Cadou& c) {
+		cout << "\nIntroduceti denumire: ";
+		in >> c.denumire;
+		cout << "Introduceti pret: ";
+		in >> c.pret;
+		cout << "Introduceti ambalaj: ";
+		in >> c.ambalaj;
+		return in;
+	}
+};
