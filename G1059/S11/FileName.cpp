877a3e16d91d35bbf5e4895abd46b02587661a37
Madalina Zurini <madalina.zurini@csie.ase.ro>
2024-12-14 00:27:40 +0200

    S11

--- G1059/S11/FileName.cpp
+++ G1059/S11/FileName.cpp
@@ -33,7 +33,13 @@ public:
 	}
 
 	float getPret() {
+		return this->pret;
 		//to do
+	}
+
+protected:
+	void setPret(float _pret) {
+		this->pret = _pret;
 	}
 };
 
@@ -42,28 +48,124 @@ public:
 
 //daca se achizitioneaza un Box format din alte Cadouri
 //atunci se aplica o reducere de 10% pe total
-class Box: public Cadou {
-	//enumeratie culoare 
+enum Culoare {
+	NA, ROSU, GALBEN, NEGRU, GRI
+};
+
+class Box : public Cadou {
+	Culoare culoare;
 	int nrCadouri;
 	Cadou* listaCadouri;
 	string textFelicitare;
 
 public:
-	float getPret() {
-		// se aplica o reducere de 10% pe totalul preturilor
-	}
 
 	//constructor fara param
-	
+	Box() :Cadou() {
+		this->culoare = Culoare::NA;
+		this->nrCadouri = 0;
+		this->listaCadouri = nullptr;
+		this->textFelicitare = "-";
+	}
 	//constructor cu toti fara vector + dimensiune
+	Box(Culoare _culoare, string _textFelicitare, string _denumire) :Cadou(_denumire, 0) {
+		this->culoare = _culoare;
+		this->textFelicitare = _textFelicitare;
+		this->nrCadouri = 0;
+		this->listaCadouri = nullptr;
+	}
+
 	//constructor copiere
+	Box(const Box& b) :Cadou(b) {
+		this->culoare = b.culoare;
+		this->textFelicitare = b.textFelicitare;
+		if (b.nrCadouri > 0 && b.listaCadouri != nullptr) {
+			this->nrCadouri = b.nrCadouri;
+			delete[]this->listaCadouri;
+			this->listaCadouri = new Cadou[this->nrCadouri];
+			for (int i = 0; i < this->nrCadouri; i++)
+				this->listaCadouri[i] = b.listaCadouri[i];
+		}
+		else {
+			this->nrCadouri = 0;
+			this->listaCadouri = nullptr;
+		}
+	}
 	//op=
+	Box operator=(const Box& b) {
+		if (this != &b) {
+			Cadou::operator=(b);
+			if (this->listaCadouri != nullptr) {
+				delete[]this->listaCadouri;
+				this->listaCadouri = nullptr;
+			}
+			this->culoare = b.culoare;
+			this->textFelicitare = b.textFelicitare;
+			if (b.nrCadouri > 0 && b.listaCadouri != nullptr) {
+				this->nrCadouri = b.nrCadouri;
+				delete[]this->listaCadouri;
+				this->listaCadouri = new Cadou[this->nrCadouri];
+				for (int i = 0; i < this->nrCadouri; i++)
+					this->listaCadouri[i] = b.listaCadouri[i];
+			}
+			else {
+				this->nrCadouri = 0;
+				this->listaCadouri = nullptr;
+			}
+
+		}
+		return *this;
+	}
 	//destructor
+	~Box() {
+		if (this->listaCadouri != nullptr) {
+			delete[]this->listaCadouri;
+			this->listaCadouri = nullptr;
+		}
+	}
 	//ostream
+	friend ostream& operator<<(ostream& out, const Box& b) {
+		out << (Cadou)b;
+		out << "\nCuloarea: " << b.culoare;
+		out << "\nText Felicitare: " << b.textFelicitare;
+		out << "\nNumar cadouri: " << b.nrCadouri;
+		out << "\nLista Cadouri: ";
+		for (int i = 0; i < b.nrCadouri; i++) {
+			out << b.listaCadouri[i] << " ";
+		}
+		return out;
+	}
 	//set si get pe vector + dimensiune
+	Cadou* getLista() {
+		return this->listaCadouri;
+	}
+
+	void setLista(int _nrCadouri, Cadou* _listaCadouri) {
+		int totalPret = 0;
+		if (_nrCadouri > 0 && _listaCadouri != nullptr) {
+			delete[]this->listaCadouri;
+			this->nrCadouri = _nrCadouri;
+			this->listaCadouri = new Cadou[this->nrCadouri];
+			for (int i = 0; i < this->nrCadouri; i++) {
+				this->listaCadouri[i] = _listaCadouri[i];
+				totalPret += this->listaCadouri[i].getPret();
+			}
+		}
+		totalPret *= 0.9;
+		this->setPret(totalPret);
+	}
 	//op + care adauga un nou Cadou in vector
 };
 
 int main() {
+	Cadou c1("Jucarie", 120);
+	Cadou c2("Joc", 30);
+	Cadou c3("Puzzle", 60);
+	Cadou lista[] = { c1,c2,c3 };
+	Box b1(Culoare::ROSU, "Felicitari", "Box Magic");
+	cout << b1;
+	b1.setLista(3, lista);
+	cout << endl << b1;
+	cout << b1.getPret();
 	return 0;
 }
