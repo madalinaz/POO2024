3ff3c3a9a0ac779fc3d9cffdf5c25bd02b991285
Madalina Zurini <madalina.zurini@csie.ase.ro>
2024-12-14 00:36:28 +0200

    S11

--- G1060/S11/FileName.cpp
+++ G1060/S11/FileName.cpp
@@ -1,19 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//design pattern-ul composite
-
+// design pattern composite
 class Cutie {
-	string material="Carton";
-	float greutate=0;
+	string material = "carton";
+	float greutate = 0;
 
 public:
-	Cutie() {
-	}
+	Cutie() {}
 
-	Cutie(string _material, float _greutate) {
-		this->material = _material;
-		this->greutate = _greutate;
+	Cutie(string _material, float _greutate) : material(_material), greutate(_greutate) {
+
 	}
 
 	Cutie(const Cutie& c) {
@@ -21,46 +18,132 @@ public:
 		this->greutate = c.greutate;
 	}
 
+	float getGreutate() {
+		return this->greutate;
+	}
+
+	void setGreutate(float _greutate) {
+		this->greutate = _greutate;
+	}
+
 	Cutie& operator=(const Cutie& c) {
-		if (this != &c) {
-			this->material = c.material;
-			this->greutate = c.greutate;
-		}
+		if (this == &c)
+			return *this;
+		this->material = c.material;
+		this->greutate = c.greutate;
 		return *this;
 	}
 
-	friend ostream& operator<<(ostream& out, const Cutie& c) {
-		out << "\nMaterial: " << c.material;
-		out << "\nGreutate: " << c.greutate;
+	friend ostream& operator<<(ostream& out, Cutie c) {
+		out << c.material << " " << c.greutate;
 		return out;
 	}
 };
 
-//Container is a Cutie
-//Container has a lista de cutii
-
-class Container: public Cutie {
+// Container is a Cutie
+// Container has a list of Cutie
+class Container : public Cutie {
 	string adresaLivrare;
 	int nrCutii;
 	Cutie* listaCutii[100];
 	float greutateMaximaAdmisa;
-
 public:
-	//constructor fara param
-	//contructor cu param mai putin vector + dim
-	// contructor copiere
-	// op=
+	// constructor fara param
+	Container() : Cutie() {
+		this->adresaLivrare = "null";
+		this->nrCutii = 0;
+		this->greutateMaximaAdmisa = 0;
+	}
+	// constructor cu param, fara vect si sim
+	Container(string _material, float _greutate, string _adresaLivrare, float _greutateMaximaAdmisa) : Cutie(_material, _greutate) {
+		this->adresaLivrare = _adresaLivrare;
+		this->nrCutii = 0;
+		this->greutateMaximaAdmisa = _greutateMaximaAdmisa;
+	}
+	// constructor copiere
+	Container(const Container& c) : Cutie(c) {
+		this->adresaLivrare = c.adresaLivrare;
+		this->greutateMaximaAdmisa = c.greutateMaximaAdmisa;
+		if (c.nrCutii <= 0)
+			this->nrCutii = 0;
+		else {
+			this->nrCutii = c.nrCutii;
+			for (int i = 0; i < this->nrCutii; i++)
+				this->listaCutii[i] = new Cutie(*c.listaCutii[i]);
+		}
+
+
+	}
+
 	// destructor
+	~Container() {
+		if (this->nrCutii > 0) {
+			for (int i = 0; i < this->nrCutii; i++) {
+				delete this->listaCutii[i];
+				this->listaCutii[i] = nullptr;
+			}
+		}
+	}
+	// op=
+	Container& operator=(Container c) {
+		if (this == &c)
+			return *this;
+
+		Cutie::operator=(c);
+		if (this->nrCutii > 0) {
+			for (int i = 0; i < this->nrCutii; i++) {
+				delete this->listaCutii[i];
+				this->listaCutii[i] = nullptr;
+			}
+		}
+		this->adresaLivrare = c.adresaLivrare;
+		this->greutateMaximaAdmisa = c.greutateMaximaAdmisa;
+		if (c.nrCutii <= 0)
+			this->nrCutii = 0;
+		else {
+			this->nrCutii = c.nrCutii;
+			for (int i = 0; i < this->nrCutii; i++)
+				this->listaCutii[i] = new Cutie(*c.listaCutii[i]);
+		}
+	}
 	// ostream
-	//op += care adauga o noua Cutie + verif pentru greutate + aruncare de exception
-	//constructor cu toti param
-	//set si get 
-	//get pe vector 
-	//get pe un elem din vector get(index)
-	//get pe dim vector
-	//set vector + dim
+	friend ostream& operator<<(ostream& out, const Container& c) {
+		out << " " << c.adresaLivrare << " " << c.greutateMaximaAdmisa << " " << c.nrCutii << " ";
+		for (int i = 0; i < c.nrCutii; i++)
+			out << *c.listaCutii[i];
+		return out;
+	}
+
+	// getter greutate suprascris
+	float getGreutate() {
+		float sum = 0;
+		for (int i = 0; i < this->nrCutii; i++)
+			sum += this->listaCutii[i]->getGreutate();
+		return sum;
+	}
+
+	// op+= - adauga o noua cutie si verifica pentru greutate maxima admisa, arunca exceptie
+	void operator+=(Cutie c) {
+		if (this->nrCutii >= 99)
+			throw new exception("deja full - nr cutii");
+
+		if (this->getGreutate() + c.getGreutate() > this->greutateMaximaAdmisa)
+			throw new exception("deja full - greutate");
+
+		this->listaCutii[this->nrCutii] = new Cutie(c);
+		this->nrCutii++;
+
+	}
 };
 
+
+
 int main() {
+	Cutie c1("carton", 2);
+	Cutie c2("plastic", 10);
+	Container container("metal", 20, "China", 200);
+	container += c1;
+	container += c2;
+	cout << container;
 	return 0;
 }
