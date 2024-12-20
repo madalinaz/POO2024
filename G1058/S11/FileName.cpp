600ae1cc4caadbf987b58df1854adcadcad4e8a0
Madalina Zurini <madalina.zurini@csie.ase.ro>
2024-12-20 05:49:48 +0200

    S11

--- G1058/S11/FileName.cpp
+++ G1058/S11/FileName.cpp
@@ -38,30 +38,156 @@ public:
 	float calculSalariu() {
 		return this->salariuBaza;
 	}
+	bool operator==(const Angajat& a)const {
+		return this->nume == a.nume;
+	}
 
 };
+//Manager is an angajat
+//Manager has a lista angajati(vector alocat dinamic de obiecte de tip angajat)
 
-//Manager is a Angajat
-//Manager has a lista Angajat (vector alocat dinamic de obj tip Angajat)
-class Manager : public Angajat {
+class Manager :public Angajat {
 	int nrSubalterni;
 	Angajat* listaAngajati;
 	int grad;//1-5
 
 public:
-	//constructor fara param
-	//constructor cu param mai putin vector si dimensiune
-	//constructor de copiere
-	//op=
-	//destructor
-	//ostream
-	//op += care populeaza vectorul listaAngajati(fara validari)
-	//op += care populeaza vectorul listaAngajati(cu validari unicitate pe nume)
+	//Constructor fara param
+	Manager() {
+		this->nrSubalterni = 0;
+		this->listaAngajati = nullptr;
+		this->grad = 0;
 
-	//constructor cu toti param(adica primeste si un vector + dim)
+	}
+	//Constructor cu param, mai putin vector si dim
+	Manager(string _nume, float _salariuBaza, int _grad) :Angajat(_nume, _salariuBaza) {
+		this->nrSubalterni = 0;
+		this->listaAngajati = nullptr;
+		/*this->nume = _nume;
+		if (_salariuBaza > 0)
+			this->salariuBaza = _salariuBaza;
+		else
+			this->salariuBaza = 0;*///Asa nu
+		if (_grad >= 0 && _grad <= 5)
+			this->grad = _grad;
+		else
+			this->grad = 1;
+
+	}
+	//constructor de copiere
+	Manager(const Manager& m) :Angajat(m) {
+		/*this->nume = m.nume;
+		if (m.salariuBaza > 0)
+			this->salariuBaza = m.salariuBaza;
+		else
+			this->salariuBaza = 0;*/
+		this->nrSubalterni = m.nrSubalterni;
+		this->listaAngajati = new Angajat[this->nrSubalterni];
+		for (int i = 0; i < this->nrSubalterni; i++)
+			this->listaAngajati[i] = m.listaAngajati[i];
+		if (m.grad >= 0 && m.grad <= 5)
+			this->grad = m.grad;
+		else
+			this->grad = 1;
+	}
+	//operator =
+	Manager& operator=(const Manager& m) {
+		if (this != &m) {
+			Angajat::operator=(m);
+			if (this->listaAngajati != nullptr)
+			{
+				delete[]this->listaAngajati;
+				this->listaAngajati = nullptr;
+			}
+			this->nrSubalterni = m.nrSubalterni;
+			this->listaAngajati = new Angajat[this->nrSubalterni];
+			for (int i = 0; i < this->nrSubalterni; i++)
+				this->listaAngajati[i] = m.listaAngajati[i];
+			if (m.grad >= 0 && m.grad <= 5)
+				this->grad = m.grad;
+			else
+				this->grad = 1;
+
+		}
+		return*this;
+	}
+	//destructor
+	~Manager() {
+		if (this->listaAngajati != nullptr)
+		{
+			delete[]this->listaAngajati;
+			this->listaAngajati = nullptr;
+		}
+	}
+	//ostream
+	friend ostream& operator<<(ostream& out, Manager& m)
+	{
+		out << "\n--------------------\n";
+		out << (Angajat)m;
+		out << "\nnr subalterni:" << m.nrSubalterni;
+		out << "\nsubalterni:";
+		for (int i = 0; i < m.nrSubalterni; i++)
+		{
+			out << "\n" << (Angajat)m.listaAngajati[i];
+		}
+		out << "\ngradul angajatului:" << m.grad;
+		out << "\n-------------------------\n";
+		return out;
+	}
+	//Operator +=(fara validari)
+	/*Manager& operator+=(const Angajat& a) {
+		Manager copie = *this;
+		delete[]this->listaAngajati;
+		this->nrSubalterni++;
+		this->listaAngajati = new Angajat[this->nrSubalterni];
+		for (int i = 0; i < this->nrSubalterni - 1; i++)
+			this->listaAngajati[i] = copie.listaAngajati[i];
+		this->listaAngajati[this->nrSubalterni-1] = a;
+		return *this;
+	}*/
+	//Operator +=(cu validari+ unicitate(nenorocire))
+	Manager& operator+=(const Angajat& a) {
+		Manager copie = *this;
+		bool OK = 1;
+		for (int i = 0; i < copie.nrSubalterni; i++)
+		{
+			if (copie.listaAngajati[i] == a)
+				OK = 0;
+		}
+		if (OK == 1) {
+			delete[]this->listaAngajati;
+			this->nrSubalterni++;
+			this->listaAngajati = new Angajat[this->nrSubalterni];
+
+
+			for (int i = 0; i < this->nrSubalterni - 1; i++)
+				this->listaAngajati[i] = copie.listaAngajati[i];
+
+			this->listaAngajati[this->nrSubalterni - 1] = a;
+		}
+
+		return *this;
+	}
 };
 
-int main() {
 
+int main() {
+	Manager m1;
+	cout << m1;
+	Manager m2("andrei", 6200, 3);
+	cout << m2;
+	m1 = m2;
+	cout << m1;
+	Manager m3("Ionescu", 220000, 5);
+	m1 = m3;
+	cout << m3;
+	Angajat a1("gigel", 222.23);
+	m1 += a1;
+	cout << m1;
+	Angajat a2("costelou", 7100.2);
+	m1 += a2;
+	Angajat a3("gigel", 2223);
+	m1 += a3;
+	cout << m1;
 	return 0;
 }
