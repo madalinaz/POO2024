a6efc4b5bc37763023ad2d42f2a6992aeb1aacae
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-01-13 11:24:13 +0200

    S13

--- G1066/S13/FileName.cpp
+++ G1066/S13/FileName.cpp
@@ -29,6 +29,7 @@ public:
 		this->nume = _nume;
 	}
 
+	//implementare concreta a meth virtuale pure din clasa de baza
 	void striga() {
 		cout << "\nActorul " + this->nume << " striga";
 	}
@@ -36,10 +37,54 @@ public:
 	void seEnerveaza() {
 		cout << "\nActorul " + this->nume << " tranteste usa";
 	}
+};
 
+//clasa abstracta
+class Animal :public  IPersonaj {
+protected:
+	string denumire;
+
+public:
+	Animal(string _denumire) {
+		this->denumire = _denumire;
+	}
+
+	//implementare concreta a meth virtuale pure din clasa de baza
+	void seEnerveaza() {
+		cout << "\nAnimalul " + this->denumire << " se enerveaza";
+	}
+
+	//cealalta meth (striga) ramane meth virtuala pura => clasa Animal inca este clasa abstracta
+	
+};
+
+//get
+//friend
+//protected
+
+class Pisica :public Animal {
+	void striga() {
+		cout << "\nPisica " + this->denumire << " miauna";
+	}
+};
+
+class Caine :public Animal {
+	void striga() {
+		cout << "\nCainele " + this->denumire << " latra";
+	}
 };
 
 int main() {
-	Actor a("Gigel");
+	Actor a1("Gigel");
+	//a1.striga();
+	//a1.seEnerveaza();
+
+	Actor* pa2 = new Actor("Costel");
+
+	//apel sceneta cu 2 personaje tip Actor
+	scenetaI(&a1, pa2);
+
+	//Animal animal1("Grivei");
+	Animal* pAnimal = new Pisica("Oscar");
 	return 0;
 }
